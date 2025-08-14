7ace60b4799aa117e07f923c8dfe30a08be12ab4
vishnu5070 <vishnuvardhan5070@gmail.com>
2025-08-14 12:29:28 +0530

    Create star5.java

---
+++ star5.java
@@ -0,0 +1,24 @@
+class star {
+    public static void main(String[] args) {
+        int a=6;
+        for (int i=0;i<=a;i++ ){
+            for(int j=6;j>=i;j--){
+                System.out.print("   ");
+            }
+            for(int j=0;j<=i*2;j++){
+                System.out.printf(" * ");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<=a;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("   ");
+            }
+            for(int j=12;j>=i*2;j--){
+                System.out.printf(" * ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
