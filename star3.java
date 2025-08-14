270c77f64739b148344f2eac997567b6df15153f
vishnu5070 <vishnuvardhan5070@gmail.com>
2025-08-14 10:59:20 +0530

    Update star3.java

--- star3.java
+++ star3.java
@@ -1,60 +1,3 @@
-/*class Main {
-    public static void main(String[] args) {
-        int a=10;
-        for (int i=0;i<=a;i++ ){
-            for(int j=0;j<=i;j++){
-                System.out.printf("* ");
-            }
-                System.out.println();
-        }
-        
-       System.out.println("Ravi is Good Boy");
-    }
-}*/
-
-/*class main {
-    public static void main(String[]args){
-        int a=10;
-        for (int i=0;i<=a;i++){
-            if(i%2==0){
-                System.out.println("* *");
-            }
-            else{
-                System.out.println("   * *");
-            }
-        }
-    }
-}*/
-/*class main {
-    public static void main(String[]args){
-        int n=1;
-        int spaces=2*(n-1);
-        int stars=0;
-        for (int i=0;i<=2*(n-1);i++ ){
-        if(i<=n){
-            spaces=spaces-2;
-            stars++;
-        }
-        else{
-            spaces=spaces+2;
-            stars--;
-        }
-            for(int j=1;j<=stars;j++){
-                System.out.printf("*");
-            }
-        }
-        for (int j=0;j<=spaces;j++){
-            System.out.println("");
-        }
-        for(int j=1;j<=stars;j++){
-            if(j!=n){
-                System.out.printf("*");
-            }
-        }
-        System.out.println();
-        
-    }
-}*/
 class star3 {
     public static void main(String[] args) {
         int a=10;
@@ -76,4 +19,5 @@ class star3 {
         }
     }
 }
+
 
