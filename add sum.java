b868e3dc73c5b986295cc0c16493596c98dbb032
vishnu vardhan <vishnuvardhan5070@gmail.com>
2025-10-22 20:45:39 +0530

    Create add sum.java

---
+++ add sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i=size[arr[]];i++){
+            for(int j=0;j=size[arr[]];j++){
+                if(arr[i]+arr[j]=target){
+                    System.out.println(nums[i],nums[j]);
+                }
+            }
+        }
+    }
+    public static void main(){
+        int[] nums;
+        int[] nums = {2,7,11,15};
+        
+
+    }
+
+
+}
